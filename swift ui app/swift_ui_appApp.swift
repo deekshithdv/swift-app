1fd0ba663dff77b6914ea73302d00d119796df98
Deekshith Vijaykumar <deekshith-dv@ibjapan.jp>
2022-11-10 10:54:20 +0900

    Initial Commit

---
+++ swift ui app/swift_ui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_appApp.swift
+//  swift ui app
+//
+//  Created by ビジャイクマール デックシット ディッバダハッリ on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
